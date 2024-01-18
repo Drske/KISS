5047b0d142438ef01cb4492beaf5f68123f2b816
mdziecielski <dziecielski.m@gmail.com>
2024-01-19 00:11:30 +0100

    Implement sampling based on dino features

--- src/kiss/sampler/__init__.py
+++ src/kiss/sampler/__init__.py
@@ -1,4 +1,5 @@
 from .random import RandomSampler
 from .kmeans import KMeansSampler
+from .kmeans_dino import KMeansDinoSampler
 from .kmeans_numerous import KMeansNumerousClusters
 from .optics import OpticsSampler
